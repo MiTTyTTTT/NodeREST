4a23e560f34312ebb3ff947d3e003e77099c1a82
MiTTyTTTT <s6406021622044@email.kmutnb.ac.th>
2024-02-12 10:39:30 +0700

    d

--- index.js
+++ index.js
@@ -35,19 +35,6 @@ app.get('/books/:id', (req, res) => {
         }
     });
 });
-/*
-app.post('/books', (req, res) => {
-    const book = req.body;
-    db.run('INSERT INTO books (title, author) VALUES (?, ?)', book.title, book.author, function (err) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            book.id = this.lastID;
-            res.send(book);
-        }
-    });
-});
-*/
 
 app.post('/books', (req, res) => {
     const book = req.body;
